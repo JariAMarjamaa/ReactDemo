541928bbe351cceb61d283159341bf0d24a7cf2f
Jari Marjamaa <jari.a.marjamaa@gmail.com>
2023-12-09 11:10:45 +0200

    Projektin pohja

---
+++ demo_project/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import Profiilikuva from './images/Profiilikuva.jpg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Profiilikuva} className="App-logo" alt="logo" />
+        <p>
+          Tikku-Ukon uudet harraste seikkailut
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
