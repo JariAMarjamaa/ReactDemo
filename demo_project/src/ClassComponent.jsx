e12c9d42e6576ef6fd3e2820b9246bfa2845ae5f
Jari Marjamaa <jari.a.marjamaa@gmail.com>
2023-12-19 10:13:43 +0200

    Lisaa tauhkaa

--- demo_project/src/ClassComponent.jsx
+++ demo_project/src/ClassComponent.jsx
@@ -1,21 +1,59 @@
-import React, { useState } from 'react';
-import kuva from './images/logo.svg';
+import React /*, { useState }*/ from 'react';
+//import kuva from './images/logo.svg';
 import { mockTestData } from "./mockData/data";
 
 import './App.css';
 
-//Extendaus tekee app:sta varsinaisen react componentin
+class Form extends React.Component {
+    render() {
+        return (
+            <form action="">
+                <input type="text" placeholder="GitHub username"/>
+                <button>Add card</button>
+            </form>
+        );
+    }
+}
+
+const CardList = (props) => {
+    return ( 
+        <div>
+            {/* instassi 1}
+            <Card
+                avatar_url={mockTestData[0].avatar_url} 
+                name={mockTestData[0].name} 
+                company={mockTestData[0].company}>
+            </Card> 
+            
+            {// instassi 2}
+            <Card { ...mockTestData[1]}></Card>
+
+            */}
+
+            {/* mockTestData on clobal muuttuja
+                map loopin sisään kaikki
+                mockTestData.map(profile => <Card {...profile}></Card> ) */}
+
+            {props.profiles.map(profile => <Card {...profile}></Card> ) }
+
+            {/*=> [ <Card/>, <Card/>, <Card/>] */}
+            {/*=> [ React.createElement(), React.createElement(), React.createElement()] */}
+        </div>
+    );
+}
+
+//Extendaus tekee Card:sta varsinaisen react componentin
 class Card extends React.Component {
 
     render () {
         
-        const profile = mockTestData[0];
-        
+       // const profile = mockTestData[0]; => this.props
+       const profile = this.props;
+
         return ( 
             // css-style sheetin sijaan, voidaan käyttää myös inline tyyli-propsia,
             //jolle annetaan parametriksi javascript objekt, joka sisältää json-elementtejä
             <div className="github-profile" style={{margin: "1rem"}}> 
-                Class-Card:
                 <img src={profile.avatar_url} alt="profilePicture"></img>
                 <div className="info">
                     <div className="name" style={{ color: Math.random() < 0.5 ? 'green': 'red' }}>
@@ -30,24 +68,24 @@ class Card extends React.Component {
 }
 
 export class HelpApp extends React.Component {
-    // constructor
+    // constructor => kutsutaan aina, kun komponentti instassi luodaan/kutsutaan
+    constructor(props) {
+        super(props);       // javescript funktio, joka pitää lisätä jotta HelpApp class linkittyy classiin, 
+        this.state = {      // mistä se on extendattu => muuten tulee virhe
+            profiles: mockTestData
+        };    
+    }
     // this
 
     render () {
         return(
             <div>
-                Class HelpApp:
                 <div className="header"> {this.props.title} </div>
-                <CardList/>
+                <Form />
+                {/* Halutaan, että CardList komponetti luodaan/kutsutaan, 
+                    vain, kun uusi kortti on luotu, ei joka kerta*/}
+                <CardList profiles= {this.state.profiles}/>
             </div>
-        )};
-}
-
-const CardList = (props) => {
-    <div>
-        Function CardList:
-        <Card>
-
-        </Card>
-    </div>
+        )
+    };
 }
